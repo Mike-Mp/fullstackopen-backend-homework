f999e44e44c0d6b0195b52c328b5402688915e65
Mike-Mp <devmichalis@protonmail.com>
2020-05-16 19:41:23 +0300

    complete 3.1 - 3.6

---
+++ dist/index.dev.js
@@ -0,0 +1,84 @@
+"use strict";
+
+var express = require("express");
+
+var app = express();
+app.use(express.json());
+var persons = [{
+  name: "George",
+  id: 1
+}, {
+  name: "Smith",
+  id: 2
+}, {
+  name: "Mike",
+  id: 3
+}, {
+  name: "Bill",
+  id: 4
+}];
+app.get("/", function (req, res) {
+  res.send("Hello world");
+});
+app.get("/api/persons", function (req, res) {
+  res.json(persons);
+});
+app.get("/api/persons/:id", function (req, res) {
+  var id = Number(req.params.id);
+  var person = persons.find(function (p) {
+    return p.id === id;
+  });
+
+  if (!person) {
+    return res.status(404).end();
+  }
+
+  res.json(person);
+});
+app.get("/info", function (req, res) {
+  var personNum = persons.length;
+  res.send("Phonebook has info for ".concat(personNum, " people.\n ").concat(new Date().toString()));
+});
+app.post("/api/persons", function (req, res) {
+  var id = Math.floor(Math.random() * 100);
+  var body = req.body;
+  var personExist = persons.find(function (p) {
+    return p.name === body.name;
+  });
+  var newPerson = {
+    name: body.name,
+    id: id
+  };
+
+  if (!body.name) {
+    return res.status(304).json({
+      error: "Name has to be included."
+    });
+  } else if (personExist) {
+    return res.status(304).json({
+      error: "Name already exists"
+    });
+  }
+
+  persons = persons.concat(newPerson);
+  res.json(newPerson);
+});
+app["delete"]("/api/persons/:id", function (req, res) {
+  var id = Number(req.params.id);
+  var person = persons.find(function (p) {
+    return p.id === id;
+  });
+
+  if (!person) {
+    res.status(404).end();
+  }
+
+  persons = persons.filter(function (p) {
+    return p !== person;
+  });
+  res.status(204).end();
+});
+var PORT = 3001;
+app.listen(PORT, function () {
+  console.log("App is listening on port ".concat(PORT));
+});
---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    name: "George",
+    id: 1,
+  },
+  {
+    name: "Smith",
+    id: 2,
+  },
+  {
+    name: "Mike",
+    id: 3,
+  },
+  {
+    name: "Bill",
+    id: 4,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (!person) {
+    return res.status(404).end();
+  }
+  res.json(person);
+});
+
+app.get("/info", (req, res) => {
+  const personNum = persons.length;
+
+  res.send(
+    `Phonebook has info for ${personNum} people.\n ${new Date().toString()}`
+  );
+});
+
+app.post("/api/persons", (req, res) => {
+  const id = Math.floor(Math.random() * 100);
+  const body = req.body;
+  const personExist = persons.find((p) => p.name === body.name);
+
+  const newPerson = {
+    name: body.name,
+    id,
+  };
+
+  if (!body.name) {
+    return res.status(304).json({
+      error: "Name has to be included.",
+    });
+  } else if (personExist) {
+    return res.status(304).json({
+      error: "Name already exists",
+    });
+  }
+
+  persons = persons.concat(newPerson);
+  res.json(newPerson);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (!person) {
+    res.status(404).end();
+  }
+  persons = persons.filter((p) => p !== person);
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
